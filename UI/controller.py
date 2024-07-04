8956212374a9f90f811c6435b22530c50f7897b1
Gabriele Sara <s287949@studenti.polito.it>
2024-07-04 09:08:57 +0200

    soluzione

--- UI/controller.py
+++ UI/controller.py
@@ -11,57 +11,39 @@ class Controller:
         self._model = model
 
     def fillDD(self):
-        country = self._model.listCountry
-        years = self._model.listYears
+        for n in self._model.getNazioni():
+            self._view._ddCountry.options.append(ft.dropdown.Option(n))
 
-        for c in country:
-            self._view._ddCountry.options.append(ft.dropdown.Option(c))
+        for a in self._model.getAnno():
+            self._view._ddyear.options.append(ft.dropdown.Option(a))
 
-        for y in years:
-            self._view._ddyear.options.append(ft.dropdown.Option(y))
+
+
 
     def handle_graph(self, e):
-        if self._view._ddCountry.value == None:
-            self._view.create_alert("Inserire un paese")
-            return
-
-        if self._view._ddyear.value == None:
-            self._view.create_alert("Inserire un anno")
-            return
-        country = self._view._ddCountry.value
-        year = self._view._ddyear.value
-        self._model.buildGraph(country, year)
-        nN, nE = self._model.getGraphSize()
+        self._model._grafo.clear()
+        nazione = self._view._ddCountry.value
+        anno = self._view._ddyear.value
         self._view.txtOut.clean()
-        self._view.txtOut.controls.append(ft.Text(f"Grafo creato con {nN} vertici e {nE} archi"))
-        self._view.btn_search.disabled = False
+        self._model.creaGrafo(nazione, anno)
+        self._view.txtOut.controls.append(ft.Text("Grafo creato"))
+        numNodi= self._model.getNumNodi()
+        numArchi = self._model.getNumArchi()
+        self._view.txtOut.controls.append(ft.Text(f"Ci sono {numNodi} vertici"))
+        self._view.txtOut.controls.append(ft.Text(f"Ci sono {numArchi} archi"))
+
+
         self._view.update_page()
+
+
+
+
+
+
+
 
     def handle_volumi(self, e):
-        volumi = self._model.getVolumi()
-        self._view.txtOut.clean()
-        self._view.txtOut.controls.append(ft.Text(f"Il volume di vendita dei retailers nel grafo è:"))
-        for v in volumi:
-            self._view.txtOut.controls.append(ft.Text(f"{v[0]} --> {v[1]}"))
-
-        self._view.update_page()
+        pass
 
     def handle_search(self, e):
-        try:
-            nMax = int(self._view._txtIn.value)
-        except ValueError:
-            self._view.create_alert("Inserire un intero nel campo della lunghezza")
-            return
-
-        if nMax < 2:
-            self._view.create_alert("Inserire un valore >= a 2")
-            return
-
-        bestPath, bestWeight = self._model.getBestPath(nMax)
-        self._view.txtOut2.clean()
-        self._view.txtOut2.controls.append(ft.Text(f"Cammino di peso massimo con esattamente {nMax} archi"))
-        self._view.txtOut2.controls.append(ft.Text(f"Peso del cammino: {bestWeight}"))
-        for i in range(len(bestPath)-1):
-            self._view.txtOut2.controls.append(ft.Text(f"{bestPath[i]} --> {bestPath[i+1]}  Peso={self._model.getPesoArco(bestPath[i], bestPath[i+1])}"))
-
-        self._view.update_page()
+        pass
--- database/DAO.py
+++ database/DAO.py
@@ -5,13 +5,13 @@ from model.retailer import Retailer
 class DAO():
 
     @staticmethod
-    def getState():
+    def getNazione():
         conn = DBConnect.get_connection()
         result = []
         cursor = conn.cursor(dictionary=True)
-        query = """select distinct (Country)
-                    from go_retailers"""
-        cursor.execute(query,)
+        query = """select distinct Country 
+                    from go_retailers gr """
+        cursor.execute(query, )
         for row in cursor:
             result.append(row['Country'])
 
@@ -20,29 +20,29 @@ class DAO():
         return result
 
     @staticmethod
-    def getYears():
+    def getAnno():
         conn = DBConnect.get_connection()
         result = []
         cursor = conn.cursor(dictionary=True)
-        query = """select distinct(extract(year from `Date`)) as year
-                    from go_daily_sales"""
+        query = """select distinct year(`Date`) as anno
+                    from go_daily_sales gds """
         cursor.execute(query, )
         for row in cursor:
-            result.append(row['year'])
+            result.append(row['anno'])
 
         cursor.close()
         conn.close()
         return result
 
     @staticmethod
-    def getNode(country):
+    def getNodi(nazione):
         conn = DBConnect.get_connection()
         result = []
         cursor = conn.cursor(dictionary=True)
         query = """select *
-                    from go_retailers
-                    where Country = %s """
-        cursor.execute(query, (country,))
+                    from go_retailers gr 
+                    where gr.Country =%s """
+        cursor.execute(query, (nazione, ))
         for row in cursor:
             result.append(Retailer(**row))
 
@@ -51,24 +51,26 @@ class DAO():
         return result
 
     @staticmethod
-    def getEdge(country, year):
+    def getArchi(nazione, anno):
         conn = DBConnect.get_connection()
         result = []
         cursor = conn.cursor(dictionary=True)
-        query = """select t1.r1, t2.r2, COUNT(distinct(t1.p1)) as peso
-                    from (select gr.Retailer_code as r1, Product_number as p1
-                            from go_daily_sales gds, go_retailers gr 
-                            where gr.Retailer_code = gds.Retailer_code and gr.Country = %s and extract(year from gds.`Date`) = %s) t1,
-                        (select gr.Retailer_code as r2, Product_number as p2
-                            from go_daily_sales gds, go_retailers gr 
-                            where gr.Retailer_code = gds.Retailer_code and gr.Country = %s and extract(year from gds.`Date`) = %s) t2
-                    where t1.p1 = t2.p2 and t1.r1 < t2.r2
-                    group by t1.r1, t2.r2"""
-        cursor.execute(query, (country, year, country, year,))
+        query = """select gr1.rc1 as n1, gr2.rc2 as n2, count(*) as peso
+                    from(select gds.Retailer_code as rc1, Product_number as pn1
+                        from go_daily_sales gds,go_retailers gr 
+                        where gds.Retailer_code = gr.Retailer_code 
+                            and gr.Country =%s
+                            and year(gds.`Date` )=%s) gr1,
+                        (select gds.Retailer_code as rc2, gds.Product_number as pn2
+                        from go_daily_sales gds,go_retailers gr 
+                        where gds.Retailer_code = gr.Retailer_code 
+                            and gr.Country =%s
+                            and year(gds.`Date` )=%s) gr2
+                where gr1.pn1 = gr2.pn2 and gr1.rc1<gr2.rc2 
+                group by gr1.rc1, gr2.rc2 """
+        cursor.execute(query, (nazione,anno, nazione, anno,))
         for row in cursor:
-            result.append((row['r1'],
-                           row['r2'],
-                           row['peso']))
+            result.append((row['n1'], row['n2'], row['peso']))
 
         cursor.close()
         conn.close()
